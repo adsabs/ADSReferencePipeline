a66c1a673c124db889a5e752166784bbbb456806
thomasallen <tom.sco@gmail.com>
2026-01-05 11:21:17 -0800

    alembic upgrade add external identifier

---
+++ alembic/versions/08ca70bd6f5f_add_external_identifier.py
@@ -0,0 +1,24 @@
+"""add_external_identifier
+
+Revision ID: 08ca70bd6f5f
+Revises: e3d6e15c3b8c
+Create Date: 2026-01-05 11:16:27.454389
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '08ca70bd6f5f'
+down_revision = 'e3d6e15c3b8c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
